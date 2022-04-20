f3f343ca93d08770e4da5c7adeaa688c4b011d10
muhammadalamin23 <alamin24034@gmail.com>
2022-04-20 11:11:18 +0600

    news-portal

---
+++ js/common-scripts.js
@@ -0,0 +1,21 @@
+
+(function($){
+	$(function(){
+
+
+
+        // Phone nav click function
+        $('#burger').click(function(){
+            $('#nav').slideToggle()
+        
+        })
+
+    });
+    // End the solutin card carousel
+    
+    
+
+	
+
+})(jQuery)
+
